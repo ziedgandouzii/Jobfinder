ce5556d4b5148f04084b5bc1ffde3cd8fc5c3e25
Zied Gandouzi <ziedgandouzi@gmail.com>
2023-12-03 18:13:25 +0100

    profile4

--- components/JobsCard.jsx
+++ components/JobsCard.jsx
@@ -9,7 +9,6 @@ export default function JobsCard({job , posted}) {
     return (
         <div key={job._id} className='w-full cursor-pointer  transition-all duration-1000  md:w-5/12 m-4 border hover:shadow-xl rounded px-4 md:flex md:flex-wrap'>
             <div className='mb-4 flex  items-center justify-center py-2 '>
-                <Image width={70} height={70} className="flex rounded-full " src={"https://xsgames.co/randomusers/avatar.php?g=male"} alt="no image" />
                 <div className='flex flex-col mx-2 px-2'>
                     <h1 className='text-xl md:text-2xl font-semibold'>{job?.user.name}</h1>
                     <p className='text-xs sm:text-sm md:text-base text-gray-800'>{job?.company}</p>
@@ -19,7 +18,7 @@ export default function JobsCard({job , posted}) {
                 <div className='flex  px-2 py-2 items-center justify-center '>
                     <BsDot className='text-4xl font-extrabold text-indigo-600' />
                     <h1 className='text-lg text-gray-900'>Salary :</h1>
-                    <p className='text-base  font-semibold'>{job?.salary}$ / month</p>
+                    <p className='text-base  font-semibold'>{job?.salary} TND / month</p>
                 </div>
                 <div className='flex px-2 py-2 items-center  justify-center'>
                     <BsDot className='text-4xl font-extrabold text-indigo-600' />
